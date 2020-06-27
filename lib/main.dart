4ecf06beb8f605294fb5aedfb673d209155d6a34
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-27 16:01:09 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:routing/theme/styles.dart';
+
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: mainBlack,
+      ),
+      home: HomePage(),
+    );
+  }
+}
