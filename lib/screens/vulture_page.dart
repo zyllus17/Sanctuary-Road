1e0ed23619ade9f811d70ec226603ac378b36760
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-27 16:02:16 +0530

    Contains data related to Vulture

---
+++ lib/screens/vulture_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class VulturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('assets/vulture.png');
+  }
+}
