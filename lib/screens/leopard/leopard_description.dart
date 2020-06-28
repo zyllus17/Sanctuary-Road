8d4f736e7eb3b139c5cdd9e873d793963f9a6b01
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 17:06:19 +0530

    🎨Leopard Description

---
+++ lib/screens/leopard/leopard_description.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:math'as math;
+import '../../theme/styles.dart';
+import '../home_page.dart';
+
+class LeopardDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageOffsetNotifier>(
+      builder: (context, notifier, child) {
+        return Opacity(
+          opacity: math.max(0, 1 - 4 * notifier.page),
+          child: child,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Text(
+          'The leopard is distinguished by its well-camouflaged fur, opportunistic hunting behaviour, broad diet, and strength.',
+          style: TextStyle(fontSize: 13, color: lightGrey),
+        ),
+      ),
+    );
+  }
+}
