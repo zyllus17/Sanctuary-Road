94b91c4e168225876bfca146bcc13d2023042584
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 17:07:12 +0530

    🚀Completed Leopard Page

--- lib/screens/leopard/leopard_page.dart
+++ lib/screens/leopard/leopard_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:routing/screens/home_page.dart';
+import 'package:routing/screens/leopard/leopard_description.dart';
+import 'package:routing/screens/leopard/the_72_text.dart';
+import 'package:routing/screens/leopard/travel_description_label.dart';
 
 class LeopardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 128),
+        The72Text(),
+        SizedBox(height: 32),
+        TravelDescriptionLabel(),
+        SizedBox(height: 32),
+        LeopardDescription(),
+      ],
+    );
   }
 }
-
