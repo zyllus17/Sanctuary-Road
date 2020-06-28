9c11ecd198f7c2ed73502a6e2986d4f6e59e8232
Maruf Hassan <50977126+zyllus17@users.noreply.github.com>
2020-06-29 03:08:32 +0530

    Delete vulture_page.dart

--- lib/screens/vulture_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class VulturePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset('assets/vulture.png');
-  }
-}
