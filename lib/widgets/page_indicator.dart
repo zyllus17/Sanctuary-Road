753db20c3aa151a37d20d7eb3e01bf1f8ead70b9
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 17:09:58 +0530

    ☸ Page Indicators added

---
+++ lib/widgets/page_indicator.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/home_page.dart';
+import '../theme/styles.dart';
+
+class PageIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Consumer<PageOffsetNotifier>(
+        builder: (context, notifier, _) {
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 24),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: notifier.page.round() == 0 ? white : lightGrey,
+                    ),
+                    height: 6,
+                    width: 6,
+                  ),
+                  SizedBox(width: 8),
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: notifier.page.round() != 0 ? white : lightGrey,
+                    ),
+                    height: 6,
+                    width: 6,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+    );
+  }
+}
