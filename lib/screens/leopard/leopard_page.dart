59d67a5c9679e3fc8f5551163f4d3bbc3c782ff9
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 13:15:04 +0530

    ✨ Leopard Page

---
+++ lib/screens/leopard/leopard_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:routing/screens/home_page.dart';
+
+class LeopardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
