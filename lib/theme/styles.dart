74b483a66e6029274aa8e9e30cf76ee04dd53d50
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 17:12:30 +0530

    🎨 Theme Color of the App

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -4,4 +4,3 @@ const Color mainBlack = Color(0xFF383838);
 const Color lightGrey = Color(0xFF707070);
 const Color lighterGrey = Color(0xFFA0A0A0);
 const Color white = Color(0xFFFFFFFF);
-const Color whiteee = Color(0xFFFFFFFF);
