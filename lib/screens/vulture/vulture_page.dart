82dc6ddb95dad795cc825c0ff0ebfd4e4e9b746c
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 13:15:16 +0530

    ✨Vulture Page

---
+++ lib/screens/vulture/vulture_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VulturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+//     return Center(
+//       child: Padding(
+//         padding: const EdgeInsets.only(bottom: 80),
+//         child: Image.asset(
+//           'assets/vulture.png',
+//           height: MediaQuery.of(context).size.height / 3,
+//         ),
+//       ),
+//     );
+   }
+ }
