ccc13474df953d96ba85e2c25f660efa0dfed597
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-28 12:47:22 +0530

    Added AppBar

---
+++ lib/widgets/app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Appbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        child: Row(
+          children: <Widget>[
+            Text(
+              'SY',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+            ),
+            Spacer(),
+            Icon(Icons.menu),
+          ],
+        ),
+      ),
+    );
+  }
+}
