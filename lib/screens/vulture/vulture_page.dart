2bdf101534ea8601b4d976a5970181cbeeb295db
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-29 03:05:01 +0530

    🐦 Vulture Main Page

--- lib/screens/vulture/vulture_page.dart
+++ lib/screens/vulture/vulture_page.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:routing/screens/vulture/vulture_circle.dart';
 
 class VulturePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
-//     return Center(
-//       child: Padding(
-//         padding: const EdgeInsets.only(bottom: 80),
-//         child: Image.asset(
-//           'assets/vulture.png',
-//           height: MediaQuery.of(context).size.height / 3,
-//         ),
-//       ),
-//     );
-   }
- }
+    return Center(
+      child: VultureCircle(),
+    );
+  }
+}
