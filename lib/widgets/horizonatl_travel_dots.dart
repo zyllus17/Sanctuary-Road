7d324abfac3cfd7fb44ec4019ad5ea1501c752bb
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-29 20:32:01 +0530

    Changed Travel Dots to Horizontal Travel Dots

---
+++ lib/widgets/horizonatl_travel_dots.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:routing/screens/home_page.dart';
+import 'package:routing/theme/styles.dart';
+import 'dart:math' as math;
+
+class HorizontalTravelDots extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer2<PageOffsetNotifier, AnimationController>(
+      builder: (context, notifier, animation, child) {
+        if (animation.value == 1) {
+          return Container();
+        }
+        double spacingFactor;
+        double opacity;
+        if (animation.value == 0) {
+          spacingFactor = math.max(0, 4 * notifier.page - 3);
+          opacity = spacingFactor;
+        } else {
+          spacingFactor = math.max(0, 1 - 6 * animation.value);
+          opacity = 1;
+        }
+        return Positioned(
+          top: 128.0 + 400 + 32 + 16 + 32 + 4,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: Opacity(
+              opacity: opacity,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(left: spacingFactor * 10),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: lightGrey,
+                    ),
+                    height: 4,
+                    width: 4,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: spacingFactor * 10),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: lightGrey,
+                    ),
+                    height: 4,
+                    width: 4,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: spacingFactor * 40),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: white),
+                    ),
+                    height: 8,
+                    width: 8,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: spacingFactor * 40),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: white,
+                    ),
+                    height: 8,
+                    width: 8,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
