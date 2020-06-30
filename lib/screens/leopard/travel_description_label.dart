6e78e857f1e6b0693aab89106794431c3f2aacc8
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:01:06 +0530

    ✈️ Tracel Description 📜

--- lib/screens/leopard/travel_description_label.dart
+++ lib/screens/leopard/travel_description_label.dart
@@ -23,3 +23,22 @@ class TravelDescriptionLabel extends StatelessWidget {
     );
   }
 }
+
+@override
+Widget build(BuildContext context) {
+  return Consumer<PageOffsetNotifier>(
+    builder: (context, notifier, child) {
+      return Opacity(
+        opacity: math.max(0, 1 - 4 * notifier.page),
+        child: child,
+      );
+    },
+    child: Padding(
+      padding: const EdgeInsets.only(left: 24),
+      child: Text(
+        'Travel description',
+        style: TextStyle(fontSize: 18),
+      ),
+    ),
+  );
+}
