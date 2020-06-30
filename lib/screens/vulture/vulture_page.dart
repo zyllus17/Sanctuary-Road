d12398269d70acc4407e40725cfa9346afd98454
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:25:29 +0530

    🐦 Vulture Page

--- lib/screens/vulture/vulture_page.dart
+++ lib/screens/vulture/vulture_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:routing/screens/map/map_hider.dart';
 import 'package:routing/screens/vulture/vulture_circle.dart';
 
 class VulturePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: VultureCircle(),
+      child: MapHider(
+        child: VultureCircle(),
+      ),
     );
   }
 }
