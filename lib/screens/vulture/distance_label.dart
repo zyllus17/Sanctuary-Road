beff78a3ed7e82d226bf3d410d2161f8d2b151bf
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:12:46 +0530

    📏 Distance Label

--- lib/screens/vulture/distance_label.dart
+++ lib/screens/vulture/distance_label.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:routing/screens/map/map_animation_notifier.dart';
+import 'package:routing/screens/map/map_hider.dart';
 import 'dart:math' as math;
 import '../../theme/styles.dart';
 import '../home_page.dart';
@@ -11,7 +13,7 @@ class DistanceLabel extends StatelessWidget {
       builder: (context, notifier, child) {
         double opacity = math.max(0, 4 * notifier.page - 3);
         return Positioned(
-          top: 128.0 + 400 + 32 + 16 + 32 + 40,
+          top: topMargin(context) + mainSquareSize(context) + 32 + 16 + 32 + 40,
           width: MediaQuery.of(context).size.width,
           child: Opacity(
             opacity: opacity,
@@ -19,13 +21,15 @@ class DistanceLabel extends StatelessWidget {
           ),
         );
       },
-      child: Center(
-        child: Text(
-          '72 km',
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: white,
+      child: MapHider(
+        child: Center(
+          child: Text(
+            '72 km',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: white,
+            ),
           ),
         ),
       ),
