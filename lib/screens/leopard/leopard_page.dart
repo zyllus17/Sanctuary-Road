6893b987833f2b584d5aca88ae2ffb9f43dcfcfe
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 13:56:58 +0530

    🐆 Leopard Page 📄

--- lib/screens/leopard/leopard_page.dart
+++ lib/screens/leopard/leopard_page.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:routing/screens/home_page.dart';
 import 'package:routing/screens/leopard/leopard_description.dart';
 import 'package:routing/screens/leopard/the_72_text.dart';
 import 'package:routing/screens/leopard/travel_description_label.dart';
+import 'package:routing/screens/map/map_animation_notifier.dart';
 
 class LeopardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 128),
+      children: <Widget>[
+        SizedBox(height: topMargin(context)),
         The72Text(),
         SizedBox(height: 32),
         TravelDescriptionLabel(),
