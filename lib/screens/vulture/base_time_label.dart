9d0a3c945f5546b01774e81fb9bb8b04d1481419
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:11:46 +0530

    ⛺ Base Time Label 🕒

--- lib/screens/vulture/base_time_label.dart
+++ lib/screens/vulture/base_time_label.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:routing/screens/map/map_animation_notifier.dart';
+import 'package:routing/screens/map/map_hider.dart';
+import 'package:routing/theme/styles.dart';
 import 'dart:math' as math;
 import '../home_page.dart';
 
 class BaseTimeLabel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer2<PageOffsetNotifier,AnimationController>(
+    return Consumer2<PageOffsetNotifier, AnimationController>(
       builder: (context, notifier, animation, child) {
         double opacity = math.max(0, 4 * notifier.page - 3);
         return Positioned(
-          top: 128.0 + 32 + 16 + 44 +(1 - animation.value) * (400 + 32 - 4),
+          top: topMargin(context) +
+              32 +
+              16 +
+              44 +
+              (1 - animation.value) * (mainSquareSize(context) + 32 - 4),
           width: (MediaQuery.of(context).size.width - 48) / 3,
           right: opacity * 24.0,
           child: Opacity(
@@ -19,11 +26,17 @@ class BaseTimeLabel extends StatelessWidget {
           ),
         );
       },
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          '07:30 am',
-          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+      child: MapHider(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            '07:30 am',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w300,
+              color: lighterGrey,
+            ),
+          ),
         ),
       ),
     );
