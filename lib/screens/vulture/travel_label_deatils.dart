9d9d5de76a6c9c0e684be1d81d4887d3ed0545b1
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:23:22 +0530

    🚶  Travel Label Details 📜

--- lib/screens/vulture/travel_label_deatils.dart
+++ lib/screens/vulture/travel_label_deatils.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:routing/screens/map/map_animation_notifier.dart';
+import 'package:routing/screens/map/map_hider.dart';
 import 'dart:math' as math;
 import '../home_page.dart';
 
@@ -9,7 +11,8 @@ class TravelDetailsLabel extends StatelessWidget {
     return Consumer2<PageOffsetNotifier, AnimationController>(
       builder: (context, notifier, animation, child) {
         return Positioned(
-          top: 128.0 + (1 - animation.value) * (400 + 32 - 4),
+          top: topMargin(context) +
+              (1 - animation.value) * (mainSquareSize(context) + 32 - 4),
           left: 24 + MediaQuery.of(context).size.width - notifier.offset,
           child: Opacity(
             opacity: math.max(0, 4 * notifier.page - 3),
@@ -17,9 +20,11 @@ class TravelDetailsLabel extends StatelessWidget {
           ),
         );
       },
-      child: Text(
-        'Travel details',
-        style: TextStyle(fontSize: 18),
+      child: MapHider(
+        child: Text(
+          'Travel details',
+          style: TextStyle(fontSize: 18),
+        ),
       ),
     );
   }
