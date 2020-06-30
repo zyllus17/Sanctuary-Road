8b8a1c34c33d5bbda620dde0588b70cd02ac9ec0
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 14:11:23 +0530

    ⛺ Base Camp Label

--- lib/screens/vulture/base_camp_label.dart
+++ lib/screens/vulture/base_camp_label.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:routing/screens/map/map_animation_notifier.dart';
+import 'package:routing/screens/map/map_hider.dart';
 import 'dart:math' as math;
 import '../home_page.dart';
 
@@ -10,7 +12,11 @@ class BaseCampLabel extends StatelessWidget {
       builder: (context, notifier, animation, child) {
         double opacity = math.max(0, 4 * notifier.page - 3);
         return Positioned(
-          top: 128.0 + 32 + 16 + 4 + (1 - animation.value) * (400 + 32 - 4),
+          top: topMargin(context) +
+              32 +
+              16 +
+              4 +
+              (1 - animation.value) * (mainSquareSize(context) + 32 - 4),
           width: (MediaQuery.of(context).size.width - 48) / 3,
           right: opacity * 24.0,
           child: Opacity(
@@ -19,11 +25,16 @@ class BaseCampLabel extends StatelessWidget {
           ),
         );
       },
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          'Base camp',
-          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+      child: MapHider(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Base camp',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
         ),
       ),
     );
