dcbe69f7eb14742573309cedc1e78bfd32f1c1f6
Maruf Hassan <mrfhassan17@gmail.com>
2020-06-30 13:44:17 +0530

    🏡 Home Page completed, fits every screen

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,16 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:routing/screens/vulture/base_camp_label.dart';
 import 'package:routing/screens/vulture/distance_label.dart';
+import 'package:routing/screens/vulture/vulture_icon_label.dart';
 import 'package:routing/widgets/app_bar.dart';
 import 'package:routing/widgets/arrow_icon.dart';
-import 'package:routing/widgets/horizonatl_travel_dots.dart';
+import 'package:routing/widgets/horizontal_travel_dots.dart';
 import 'package:routing/widgets/page_indicator.dart';
 import 'package:routing/widgets/share_button.dart';
+import 'package:routing/widgets/vertical_travel_dots.dart';
 import 'dart:math' as math;
+import 'leopard/leopard_icon_label.dart';
 import 'leopard/leopard_image.dart';
 import 'leopard/leopard_page.dart';
+import 'map/curved_route.dart';
+import 'map/map_animation_notifier.dart';
+import 'map/map_base_camp.dart';
+import 'map/map_image.dart';
+import 'map/map_leopards.dart';
+import 'map/map_start_camp.dart';
+import 'map/map_vulture.dart';
 import 'vulture/base_time_label.dart';
-import 'vulture/map_button.dart';
 import 'vulture/start_camp_label.dart';
 import 'vulture/start_time_label.dart';
 import 'vulture/travel_label_deatils.dart';
@@ -40,67 +49,88 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   AnimationController _animationController;
+  AnimationController _mapAnimationController;
   final PageController _pageController = PageController();
 
-  double get maxHeight => MediaQuery.of(context).size.height;
+  double get maxHeight => mainSquareSize(context) + 32 + 24;
 
   @override
   void initState() {
+    super.initState();
     _animationController = AnimationController(
       vsync: this,
       duration: Duration(milliseconds: 1000),
     );
-    super.initState();
+    _mapAnimationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
   }
 
   @override
   void dispose() {
     _animationController.dispose();
+    _mapAnimationController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    mediaPadding = MediaQuery.of(context).padding;
     return ChangeNotifierProvider(
       builder: (_) => PageOffsetNotifier(_pageController),
       child: ListenableProvider.value(
         value: _animationController,
-        child: Scaffold(
-          body: GestureDetector(
-            onVerticalDragUpdate: _handleDragUpdate,
-            onVerticalDragEnd: _handleDragEnd,
-            child: SafeArea(
-              child: Stack(
-                alignment: Alignment.centerLeft,
-                children: [
-                  PageView(
-                    controller: _pageController,
-                    physics: ClampingScrollPhysics(),
-                    children: <Widget>[
-                      LeopardPage(),
-                      VulturePage(),
-                    ],
+        child: ChangeNotifierProvider(
+          builder: (_) => MapAnimationNotifier(_mapAnimationController),
+          child: Scaffold(
+            body: Stack(
+              children: <Widget>[
+                MapImage(),
+                SafeArea(
+                  child: GestureDetector(
+                    onVerticalDragUpdate: _handleDragUpdate,
+                    onVerticalDragEnd: _handleDragEnd,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        PageView(
+                          controller: _pageController,
+                          physics: ClampingScrollPhysics(),
+                          children: <Widget>[
+                            LeopardPage(),
+                            VulturePage(),
+                          ],
+                        ),
+                        Appbar(),
+                        LeopardImage(),
+                        VultureImage(),
+                        ShareButton(),
+                        PageIndicator(),
+                        ArrowIcon(),
+                        TravelDetailsLabel(),
+                        StartCampLabel(),
+                        StartTimeLabel(),
+                        BaseCampLabel(),
+                        BaseTimeLabel(),
+                        DistanceLabel(),
+                        HorizontalTravelDots(),
+                        MapButton(),
+                        VerticalTravelDots(),
+                        VultureIconLabel(),
+                        LeopardIconLabel(),
+                        CurvedRoute(),
+                        MapBaseCamp(),
+                        MapLeopards(),
+                        MapVultures(),
+                        MapStartCamp(),
+                      ],
+                    ),
                   ),
-                  Appbar(),
-                  LeopardImage(),
-                  VultureImage(),
-                  ShareButton(),
-                  PageIndicator(),
-                  ArrowIcon(),
-                  TravelDetailsLabel(),
-                  StartCampLabel(),
-                  StartTimeLabel(),
-                  BaseCampLabel(),
-                  BaseTimeLabel(),
-                  DistanceLabel(),
-                  HorizontalTravelDots(),
-                  MapButton(),
-                  
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
